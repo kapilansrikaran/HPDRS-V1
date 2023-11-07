c8590e19ee6b86ba105c45bcaf9ca54e05fa4a23
kapilansrikaran <kapilansrikaran@gmail.com>
2023-11-07 08:53:03 +0530

    bd

--- Demo Day.sql
+++ Demo Day.sql
@@ -29,6 +29,12 @@ Call CreateUser('Kapilan', 'tg489', 'tg489');
 Call CreateUser('Madhushan', 'tg507', 'tg507');
 Call CreateUser('Chamidu', 'tg523', 'tg523');
 Call CreateUser('Ravi', 'tg266', 'tg266');
+
+Call CreateUser('subwarden', 'subwarden', '1234');
+Call CreateUser('academicwarden', 'academicwarden', '1234');
+Call CreateUser('ssc', 'ssc', '1234');
+Call CreateUser('dean', 'dean', '1234');
+
 Select * from hostel_db.users; 
 
 
@@ -106,6 +112,10 @@ CALL InsertUserRole(2, 1);
 CALL InsertUserRole(3, 1); 
 CALL InsertUserRole(4, 1); 
 CALL InsertUserRole(5, 1); 
+CALL InsertUserRole(6, 2); 
+CALL InsertUserRole(7, 3); 
+CALL InsertUserRole(8, 4); 
+CALL InsertUserRole(9, 5); 
 
 Select * from hostel_db.users_roles;
 
@@ -214,14 +224,163 @@ Select * from property_view;
 
 -- Complaint
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CREATE TABLE complaint(
+	complaint_id INT AUTO_INCREMENT PRIMARY KEY,
+    complaint_description TEXT NOT NUll,  
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    complainer_id INT,
+    property_id INT,
+    complain_current_status ENUM('open','in-progress', 'close') NOT NULL,
+	FOREIGN KEY (complainer_id) REFERENCES users(user_id),
+    FOREIGN KEY (property_id) REFERENCES property(property_id)
+); 
+
+-- Create complaint by using procedure
+DELIMITER //
+CREATE PROCEDURE CreateComplaint(
+    IN p_complaint_description TEXT,
+    IN p_complainer_id INT,
+    IN p_property_id INT,
+    IN p_complain_current_status ENUM('open','in-progress', 'close')
+)
+BEGIN
+    INSERT INTO complaint (complaint_description, create_date, complainer_id, property_id, complain_current_status)
+    VALUES (p_complaint_description, CURRENT_TIMESTAMP, p_complainer_id, p_property_id, p_complain_current_status);
+END;
+//
+DELIMITER ;
+
+Select * from hostel_db.complaint;
+CALL CreateComplaint('Chair is broken', 2, 2, 'open');
+CALL CreateComplaint('Again Chair is broken', 2, 2, 'open');
+Select * from hostel_db.complaint;
+
+
+CREATE TABLE complaint_assignments (
+    assignment_id INT AUTO_INCREMENT PRIMARY KEY,
+    complaint_id INT,
+    role_id INT,
+    assigned_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (complaint_id) REFERENCES complaint(complaint_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+
+DELIMITER //
+CREATE PROCEDURE AssignComplaintToRole(IN complaintID INT, IN roleID INT)
+BEGIN
+    INSERT INTO complaint_assignments (complaint_id, role_id, assigned_date)
+    VALUES (complaintID, roleID, NOW());
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER AutoAssignComplaint
+AFTER INSERT ON complaint
+FOR EACH ROW
+BEGIN
+    DECLARE predefinedRoleID INT;
+
+    SET predefinedRoleID = (SELECT role_id FROM roles WHERE role_name = 'SUBWARDEN');
+
+    CALL AssignComplaintToRole(NEW.complaint_id, predefinedRoleID);
+END //
+DELIMITER ;
+
+
+-- Complant View
+CREATE VIEW complaint_view AS
+SELECT
+    c.complaint_id,
+    c.complaint_description,
+    c.create_date,
+    u.username,
+    c.complain_current_status,
+    u.username AS complainer_username,
+    p.property_name
+FROM complaint c
+LEFT JOIN users u ON c.complainer_id = u.user_id
+LEFT JOIN property p ON c.property_id = p.property_id;
+
+Select * from complaint_view;
 
 
 
 
+-- Complaint escalation 
+-- xxxxxxxxxxxxxxxxxxxx
+-- Step 1:  Stored Function to Calculate Elapsed Time
+DELIMITER //
+CREATE FUNCTION CalculateElapsedTime(complaintDate TIMESTAMP) RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE currentTime TIMESTAMP;
+    DECLARE elapsedTime INT;
+
+    SET currentTime = NOW();
+    SET elapsedTime = TIMESTAMPDIFF(DAY, complaintDate, currentTime);
+
+    RETURN elapsedTime;
+END //
+DELIMITER ;
 
 
+-- Step 2: Stored procedure to update complaint status and escalate
+DELIMITER //
+CREATE PROCEDURE UpdateComplaintStatusAndEscalate()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE complaintID INT;
+    DECLARE currentStatus ENUM('open', 'in-progress', 'close');
+    DECLARE createDate TIMESTAMP;
+    DECLARE elapsedTime INT;
+
+    -- Declare the cursor
+    DECLARE cur CURSOR FOR
+        SELECT complaint_id, complain_current_status, create_date
+        FROM complaint;
+
+    -- Define handler for when there are no more rows
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    -- Start iterating through complaints
+    read_loop: LOOP
+        FETCH cur INTO complaintID, currentStatus, createDate;
+
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        -- Calculate elapsed time
+        SET elapsedTime = CalculateElapsedTime(createDate);
+
+        -- Check escalation rules and update status
+        IF currentStatus = 'open' AND elapsedTime > 7 THEN
+            -- Escalate to sub-warden
+            UPDATE complaint SET complain_current_status = 'in-progress' WHERE complaint_id = complaintID;
+  
+        ELSEIF currentStatus = 'in-progress' AND elapsedTime > 14 THEN
+            -- Escalate to academic warden
+            UPDATE complaint SET complain_current_status = 'in-progress' WHERE complaint_id = complaintID;
+        END IF;
+    END LOOP;
+
+    CLOSE cur;
+END //
+DELIMITER ;
 
 
+-- Step 3
+DELIMITER //
+CREATE EVENT CheckComplaintEscalation
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    CALL UpdateComplaintStatusAndEscalate();
+END;
+DELIMITER ;
 
 
 
